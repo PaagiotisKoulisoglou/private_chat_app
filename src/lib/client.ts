a12e58317427451d39efb61ba8b2de0f5972d051
PanagiotisKoulisoglou <panagiotiskoulisoglou@gmail.com>
2026-03-10 02:59:00 +0200

    changed to production client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,8 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
-export const client = treaty<App>('localhost:3000').api
+const BASE_URL = typeof window !== "undefined" 
+  ? window.location.origin 
+  : process.env.NEXT_PUBLIC_APP_URL!
+
+export const client = treaty<App>(BASE_URL).api
